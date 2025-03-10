67613b94cb34fe8144e1b057d38451b0ff740f5b
ZetaChu1c <neococ233@gmail.com>
2025-03-10 19:51:55 +0800

    适用于虚拟机本地部署

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from "node:url";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
+import VueSetupExtend from "vite-plugin-vue-setup-extend";
+import { defineConfig, loadEnv } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VueDevTools(), VueSetupExtend()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
